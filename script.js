ac395d02e9640e641fb36847118a08c8c11b364c
Piotr Michalczuk <piotrekmichal91@gmail.com>
2017-02-07 20:18:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+	var panels = document.querySelectorAll('.panel');
+
+	function toggleOpen() {
+      this.classList.toggle('open');
+    }
+
+    function toggleActive(e) {
+      if (e.propertyName.includes('flex')) {
+        this.classList.toggle('active');
+      }
+    }
+
+    panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+    panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+})
